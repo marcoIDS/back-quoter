fa5787dc5fe46a336268583d440fcea5708deaf1
AlexaRos <mope0416@gmail.com>
2020-03-22 22:21:28 -0600

    Procfile y seeder

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,24 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/Role', async (faker,i,data) => {
+    return {
+       name: data.name,
+       description: data.description
+    }
+  })
+ 
+ Factory.blueprint('App/Models/User', async (faker,i,data) => {
+    return {
+       name: 'admin',
+       lastName_1: 'admin',
+       lastName_2: 'admin',
+       username: 'admin1',
+       email: 'admin@gmail.com',
+       password: data.password,
+       role_id: data.role_id,
+       telephone: '7777'
+    }
+ })
